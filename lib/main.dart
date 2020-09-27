503bee4a7809a05ccb3ae89e91913f71b6c8d9a7
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-09-27 15:21:57 +0200

    finish app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Color(0xffecf0f1),
       appBar: AppBar(
         title: Text('I AM POOR'),
+        backgroundColor: Color(0xff2c3e50),
+      ),
+      body: Center(
+        child: Image.asset('images/poorMan.png'),
       ),
     ),
   ));
